aba978bfa5048c8b6ff2f329a9a1d4bd7251875a
Shaline Raghupathy <shalineraghupathy02@gmail.com>
2025-01-09 04:24:45 +0000

    Working game logic

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 "use strict";
+let randonNumber = "";
+const generateRandomNumber = function () {
+  randonNumber = Math.trunc(Math.random(1, 20) * 20) + 1;
+};
+
+generateRandomNumber();
 
 const checkNumber = function () {
-  const guess = Number(document.querySelector(".guess").value);
+  const guess = document.querySelector(".guess").value;
   document.querySelector(".guess").value = "";
   let score = document.querySelector(".score");
   let highScore = document.querySelector(".highscore");
-  if (guess == "") {
+  if (guess == randonNumber) {
     document.querySelector(".message").textContent = "Yay!!!You are right.";
-    document.querySelector(".number").textContent = "";
+    document.querySelector(".number").textContent = randonNumber;
     if (highScore.textContent < score.textContent) {
       highScore.textContent = score.textContent;
     }
   } else {
-    if (guess > "") {
+    if (guess > randonNumber) {
       document.querySelector(".message").textContent =
         "Nahh!!!Guess is too high.";
-    } else if (guess < "") {
+    } else if (guess < randonNumber) {
       document.querySelector(".message").textContent =
         "Nahh!!!Guess is too low.";
     } else {
       document.querySelector(".message").textContent = "Not a valid number!!!";
     }
-
     score.textContent = score.textContent - 1;
-    console.log(score.value);
   }
 };
 
