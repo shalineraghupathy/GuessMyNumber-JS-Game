b6fac6c80ad45c28f6707c249becc8e6a01215f7
Shaline Raghupathy <shalineraghupathy02@gmail.com>
2025-01-09 05:44:38 +0000

    code refactor

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 let randonNumber = "";
+
 const generateRandomNumber = function () {
   randonNumber = Math.trunc(Math.random(1, 20) * 20) + 1;
   console.log(randonNumber);
@@ -7,49 +8,55 @@ const generateRandomNumber = function () {
 
 generateRandomNumber();
 
+const setContent = function (classname, message) {
+  document.querySelector(`.${classname}`).textContent = message;
+};
+
+const getContent = function (classname) {
+  return document.querySelector(`.${classname}`).textContent;
+};
+
 const checkNumber = function () {
   const guess = document.querySelector(".guess").value;
   document.querySelector(".guess").value = "";
-  let score = document.querySelector(".score");
-  let highScore = document.querySelector(".highscore");
-  if (score.textContent > 1) {
+  let score = getContent("score");
+  let highScore = getContent("highscore");
+
+  if (score > 1) {
     if (guess == randonNumber) {
       //player wins
       document.querySelector("body").style.backgroundColor = "#60b347";
-      document.querySelector(".number").style.width = "25rem";
-      document.querySelector(".message").textContent = "Yay!!!You are right.";
-      document.querySelector(".number").textContent = randonNumber;
-      if (highScore.textContent < score.textContent) {
-        highScore.textContent = score.textContent;
+      document.querySelector(".number").style.width = "30rem";
+      setContent("message", "Yay!!!You are right.");
+      setContent("number", randonNumber);
+      if (highScore < score) {
+        console.log(score);
+        setContent("highscore", score);
       }
     } else if (!guess) {
       //no number input
-      document.querySelector(".message").textContent = "Not a valid number!!!";
+      setContent("message", "Not a valid number!!!");
     } else {
       //wrong answer
-      if (guess > randonNumber) {
-        //higher number
-        document.querySelector(".message").textContent =
-          "Nahh!!!Guess is too high.";
-      } else {
-        //lower number
-        document.querySelector(".message").textContent =
-          "Nahh!!!Guess is too low.";
-      }
-      score.textContent--;
+      const message =
+        guess > randonNumber
+          ? "Nahh!!!Guess is too high."
+          : "Nahh!!!Guess is too low.";
+      setContent("message", message);
+      setContent("score", score - 1);
     }
   } else {
     //game over
-    document.querySelector(".message").textContent = "Game Over!!!!";
-    score.textContent--;
+    setContent("message", "Game Over!!!!");
+    setContent("score", score - 1);
   }
 };
 
 const resetGame = function () {
   document.querySelector(".guess").value = "";
-  document.querySelector(".score").textContent = 20;
-  document.querySelector(".number").textContent = "?";
-  document.querySelector(".message").textContent = "Start Guessing...";
+  setContent("message", "Start Guessing...");
+  setContent("score", "20");
+  setContent("number", "?");
   document.querySelector(".number").style.width = "15rem";
   document.querySelector("body").style.backgroundColor = "#222";
   generateRandomNumber();
